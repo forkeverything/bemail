48965b66fc73b63547d48c2088e60071ffb1d2e3
forkiven <mikewu1209@gmail.com>
2017-10-19 14:44:46 +0700

    trying to decode gengo controller callbakc

--- app/Http/Controllers/GengoController.php
+++ app/Http/Controllers/GengoController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class GengoController extends Controller
 {
+
+
     /**
      * Pick up the callback fromm Gengo.
      *
@@ -14,6 +17,19 @@ class GengoController extends Controller
      */
     public function postPickUp(Request $request)
     {
+        // Gengo posts the response inside a 'job' parameter
+        $body = $request->all()["job"];
+
+        $customData = json_decode($body["custom_data"], true);
+
+        $messageId = \Vinkla\Hashids\Facades\Hashids::decode('message', $body["custom_data"]["message_id"]);
+
+        \Log::info('FOR MESSAGE ID: ' . $messageId);
+
+        // Pending: Translator has begun work.
+
+        // Approved: job (completed translation)
+
         \Log::info(print_r($request->all(), true));
         return response("Got it", 200);
     }
