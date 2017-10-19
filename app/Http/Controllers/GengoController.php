8fb36aa0575d3adb531d4ee6b07681fa741ca252
forkiven <mikewu1209@gmail.com>
2017-10-19 14:47:57 +0700

    trying to parse response body from gengo

--- app/Http/Controllers/GengoController.php
+++ app/Http/Controllers/GengoController.php
@@ -20,17 +20,20 @@ class GengoController extends Controller
         // Gengo posts the response inside a 'job' parameter
         $body = $request->all()["job"];
 
-        $customData = json_decode($body["custom_data"], true);
-
-        $messageId = \Vinkla\Hashids\Facades\Hashids::decode('message', $body["custom_data"]["message_id"]);
-
-        \Log::info('FOR MESSAGE ID: ' . $messageId);
+//        \Log::info($body);
+//
+//        $customData = json_decode($body["custom_data"], true);
+//
+//        $messageId = \Vinkla\Hashids\Facades\Hashids::decode('message', $body["custom_data"]["message_id"]);
+//
+//        \Log::info('FOR MESSAGE ID: ' . $messageId);
 
         // Pending: Translator has begun work.
 
         // Approved: job (completed translation)
 
-        \Log::info(print_r($request->all(), true));
+
+
         return response("Got it", 200);
     }
 }
