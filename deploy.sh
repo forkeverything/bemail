e62bbf717462a7e20f3bf282b2aba24f914bfa62
forkiven <mikewu1209@gmail.com>
2017-10-23 15:18:56 +0700

    untracked deploy script for git

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-now="$(date +'%d/%m/%Y/%r')";
-git add .;
-git commit -m "DEPLOY LIVE AT: $now";
-git push origin master;
-echo "pushed";
-curl https://forge.laravel.com/servers/144475/sites/368191/deploy/http?token=dDjvLzLkbXcieT9dLRUuxFLHxy9rhMfgGLWMCc8Y
-
