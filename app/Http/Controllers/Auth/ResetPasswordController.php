63c1286e666b750084e3e47beed9e666770e31cd
forkiven <mikewu1209@gmail.com>
2018-02-02 13:54:53 +0700

    Changed auth redirectTo routes to /compose instead of /home

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/compose';
 
     /**
      * Create a new controller instance.
