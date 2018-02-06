d994339387da10c4e4dbf288c32b3cae40aa76d8
forkiven <mikewu1209@gmail.com>
2018-02-06 15:58:58 +0700

    DEPLOY LIVE AT: 06/02/2018/03:58:58 pm

--- resources/views/messages/compose.blade.php
+++ resources/views/messages/compose.blade.php
@@ -4,14 +4,6 @@
         @include('flash::message')
         <compose-form token="{{ csrf_token() }}"
                       word-credits="{{ Auth::user()->word_credits }}"
-                      :errors="{{ $errors }}"
-                      recipients="{{ old('recipients') }}"
-                      subject-old="{{ old('subject') }}"
-                      :languages="{{ $languages }}"
-                      :user-lang="{{ $userLang }}"
-                      lang-src-old="{{ old('lang_src') }}"
-                      lang-tgt-old="{{ old('lang_tgt') }}"
-                      body-old="{{ old('body') }}"
         ></compose-form>
     </div>
 @endsection
