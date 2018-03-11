cefb6f84e86e743acb0889a7952740365bced728
forkiven <mikewu1209@gmail.com>
2018-03-11 21:15:35 +0700

    Added SubscriptionsController

---
+++ app/Http/Controllers/SubscriptionsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubscriptionsController extends Controller
+{
+    // handles requests that  modify
+    // User subscriptions.
+}
