0e1b6f0610dc4f1f7f48a13bb923d04104c04579
forkiven <mikewu1209@gmail.com>
2018-03-14 07:28:01 +0700

    Navbar stylez.

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light mb-3 border-bottom box-shadow">
 
 	<div class="container">
 		<!-- Logo -->
