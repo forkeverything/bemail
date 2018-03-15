27ea2462b4189c4d206957dfaeb2bdfa83d2752f
forkiven <mikewu1209@gmail.com>
2018-03-15 21:15:09 +0700

    Automatically sign up dev user Mike as a Stripe customer with a car.d

--- database/seeds/development/UserMike.php
+++ database/seeds/development/UserMike.php
@@ -14,11 +14,31 @@ class UserMike extends Seeder
      */
     public function run()
     {
+
+        \Stripe\Stripe::setApiKey("sk_test_WHtMmNZ34oYmb39Qep0uacDP");
+
+        $token = \Stripe\Token::create([
+            "card" => [
+                "number" => "4242424242424242",
+                "exp_month" => 3,
+                "exp_year" => 2019,
+                "cvc" => "314"
+            ]
+        ]);
+
+        $stripeCustomer = \Stripe\Customer::create(array(
+            "description" => "mail@wumike.com",
+            "source" => $token->id
+        ))->id;
+
         User::create([
             'name' => 'Mike Wu',
             'email' => 'mail@wumike.com',
             'password' => bcrypt('password'),
-            'language_id' => 1
+            'language_id' => 1,
+            'stripe_id' => $stripeCustomer,
+            'card_brand' => $token->card->brand,
+            'card_last_four' => $token->card->last4
         ]);
     }
 }
