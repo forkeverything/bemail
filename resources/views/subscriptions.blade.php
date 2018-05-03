d5411d4c64da19adc569c80a2efc4826a52e7c6c
Mike Wu <mail@wumike.com>
2018-05-04 05:36:58 +0700

    Set up basic subscriptions view.

--- resources/views/subscriptions.blade.php
+++ resources/views/subscriptions.blade.php
@@ -2,7 +2,51 @@
 
 @section('content')
 
-	<div class="subscriptions-table">
+	<h5>Credit Card</h5>
+
+	<p>You currently don't have a credit card saved.</p>
+
+	<button type="button" class="btn btn-primary mb-5">Add Card</button>
+
+	<h5>Subscription</h5>
+
+
+	<p>Pick a subscription that suits your needs.</p>
+
+	<div class="subscription-table row">
+
+
+		<div class="col-xs-12 col-lg-4">
+			<div class="card text-center">
+				<div class="card-header">
+					<h3 class="display-2">
+						<span class="currency">$</span>
+						<span>0</span>
+						<span class="period">/month</span>
+					</h3>
+				</div>
+				<div class="card-body">
+					<h4 class="card-title">
+						Free
+					</h4>
+					<ul class="list-group">
+						<li class="list-group-item">
+							Service Charge $0.07 / Word
+						</li>
+						<li class="list-group-item">
+							Auto-Translate Replies
+						</li>
+						<li class="list-group-item">
+							Send to Self
+						</li>
+						<li class="list-group-item">
+							Send Attachments
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
 	</div>
+
 @endsection
